a369d4f26bdaf41370aff1e933ae8132922312b7
Melanie Gin <melaniegin1@gmail.com>
2014-01-31 12:27:46 -0800

    started first application, flyhappy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlyhappyApp::Application.config.secret_key_base = '1d8f25b8cb50e2614c6ad8f480f30f5b3c85dfcc49b4f8bfadc51466c4751ff20429e5e4aa2dc2f7082f57ac2ab2dd742ac4b7bdc3a54aee25074fda88e1b755'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlyhappyApp::Application.config.session_store :cookie_store, key: '_flyhappy_app_session'
