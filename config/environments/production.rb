e098a77446bda08a6843e926dd712a146b202e04
Melanie Gin <melaniegin1@gmail.com>
2014-02-04 20:32:08 -0800

    updated production.rb again...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ FlyhappyApp::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -37,7 +37,7 @@ FlyhappyApp::Application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -68,13 +68,6 @@ FlyhappyApp::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
-
-
-
-  # Serve static assets
-  config.serve_static_assets = true
-  config.assets.compile = true
-
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
