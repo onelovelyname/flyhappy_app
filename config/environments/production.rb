630fac828f9a37507dbec0a70b6a3bbf09f26bbc
Melanie Gin <melaniegin1@gmail.com>
2014-02-04 19:35:44 -0800

    changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,11 @@ FlyhappyApp::Application.configure do
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
 
+
+  # Serve static assets
+  config.serve_static_assets = true
+
+
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
